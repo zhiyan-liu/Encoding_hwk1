f73c93f5b4ceccb6039b64a37f3e238e108f0ac4
johnzja <johnzja@qq.com>
2020-10-11 22:53:45 +0800

    fix MSE bugs

--- matlab/setup_encoder.m
+++ matlab/setup_encoder.m
@@ -1,4 +1,4 @@
-conv_encoder_conf.n = 2;    % Output bits.
+conv_encoder_conf.n = 3;    % Output bits.
 conv_encoder_conf.k = 1;    % Input bits.
 conv_encoder_conf.N = 4;    
 conv_encoder_conf.window_factor=6;
@@ -7,8 +7,9 @@ conv_encoder_conf.trailing = true;
 A = cell(conv_encoder_conf.n,1);
 %       a4,a3,a2,a1
 
-A{1} = [1, 1, 0, 1];    
-A{2} = [1, 1, 1, 1];    
+A{1} = [1, 0, 1, 1];    
+A{2} = [1, 1, 0, 1]; 
+A{3} = [1, 1, 1, 1];
 
 conv_encoder_conf.A=A;
 conv_encoder_conf.loss_func = @hamming_distance;
